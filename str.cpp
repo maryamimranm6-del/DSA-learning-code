1f4055b4855ade0a13069bd2020cf39046a6bd17
Maryam-imran <maryamimranm6@gmail.com>
2025-10-07 17:37:19 +0500

    Add files via upload

---
+++ str.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string str="hello hira";
+    cout<<str<<endl;
+    string str2="tahreem";
+    string str1="Eman";
+    string res=str1+str2;
+    cout<<str2<<" "<<str1<<endl;
+    cout<<str.leng()<<endl;
+}
